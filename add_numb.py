5c9ba0fa271d754265ae6adea1672a15369e257a
Tengfei Lin <tengfei.lin@ou.edu>
2013-07-02 00:14:02 -0400

    small mofification

--- add_numb.py
+++ add_numb.py
@@ -1,10 +1,14 @@
 # This has been authored by Tengfei Lin but who knows who will comtribute to it
-def add_two(a,b):
-    print ("Hi, I'm just adding two numbers here.")
-    return a+b
+#def add_two(a,b):
+#    print ("Hi, I'm just adding two numbers here.")
+#    return a+b
+
+from adding import add_two, add_three
 
 def main():
-    print ("2+2", add_two(2,2))
+    print ("2+2=", add_two(2,2))
+    print ("2+3+4=", add_three(2,3,4))
+    print ("hello Oklahomay")
 
 if __name__ == "__main__":
     main()
